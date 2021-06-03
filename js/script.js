162e01c44ba93bca4000341d7f49f594bb78b676
gianmarco <gianmarco.palombo600@gmail.com>
2021-06-03 16:38:57 +0200

    bonus mk2 (molto meglio)

--- js/script.js
+++ js/script.js
@@ -2,43 +2,44 @@
 // sto cercando di semplificare, intanto pusho il bonus
 
 for (var i = 0; i <= 100; i++){
-    var m3 = false;
-    var m5 = false;
-    var m2 = false;
-    var nm = true;
-    if ( i % 2 == 0 && i != 0){
-        m2 = true;
+    var str ='<li>' + i;
+    if ( i % 2 == 0 && i % 3 == 0 && i != 0){
+        str +='Boolean';
     }
     if ( i % 3 == 0 && i != 0){
-        m3 = true;
+        str +='fizz';
     }
     if ( i % 5 == 0 && i != 0){
-        m5 = true;
+        str +='buzz';
     }
-    if (m2 == true || m3 == true || m5 == true){
-        if (m3 && m2 && m5){
-            document.getElementById('list').innerHTML += '<li>' + i + 'buzz' + 'fizz' + 'Boolean' + '</li>';
-            nm = m2 = m3 = m5 = false
-        }
-        if (m3 && m2){
-            document.getElementById('list').innerHTML += '<li>' + i + 'fizz' + 'Boolean' + '</li>';
-            nm = m2 = m3 = m5 = false
-        }
-        if (m3 && m5){
-            document.getElementById('list').innerHTML += '<li>' + i + 'fizz' + 'buzz' + '</li>';
-            nm = m2 = m3 = m5 = false
-        }
-        if (m3){
-            document.getElementById('list').innerHTML += '<li>' + i + 'fizz' + '</li>';
-            nm = m2 = m3 = m5 = false
-        }
-        if (m5){
-            document.getElementById('list').innerHTML += '<li>' + i + 'buzz' + '</li>';
-            nm = m2 = m3 = m5 = false
-        }
-    }
-    else if(nm == true){
-        document.getElementById('list').innerHTML += '<li>' + i + '</li>';
-    }    
-}    
+    str += '</li>'
+    document.getElementById('list').innerHTML += str;
+
+}
+//     if (m2 == true || m3 == true || m5 == true){
+//         if (m3 && m2 && m5){
+//             document.getElementById('list').innerHTML += '<li>' + i + 'buzz' + 'fizz' + 'Boolean' + '</li>';
+//             nm = m2 = m3 = m5 = false
+//         }
+//         if (m3 && m2){
+//             document.getElementById('list').innerHTML += '<li>' + i + 'fizz' + 'Boolean' + '</li>';
+//             nm = m2 = m3 = m5 = false
+//         }
+//         if (m3 && m5){
+//             document.getElementById('list').innerHTML += '<li>' + i + 'fizz' + 'buzz' + '</li>';
+//             nm = m2 = m3 = m5 = false
+//         }
+//         if (m3){
+//             document.getElementById('list').innerHTML += '<li>' + i + 'fizz' + '</li>';
+//             nm = m2 = m3 = m5 = false
+//         }
+//         if (m5){
+//             document.getElementById('list').innerHTML += '<li>' + i + 'buzz' + '</li>';
+//             nm = m2 = m3 = m5 = false
+//         }
+//     }
+//     else if(nm == true){
+//         document.getElementById('list').innerHTML += '<li>' + i + '</li>';
+//     }    
+// }    
 
